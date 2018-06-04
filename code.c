3857fbf775331c7f1789f5045cc42d18fc95ef51
Superfawll <razizi@ce.sharif.edu>
2018-06-05 03:20:38 +0430

    The Firsth Breath!

---
+++ code.c
@@ -0,0 +1,29 @@
+/* test case */
+int a = +2;
+int b = 1 - 2;
+int var1;
+int array1[5];
+
+int assign1(int a){
+	a=2;
+	output(a);
+	return 1;
+}
+void main(void){
+	int voida;
+	void int3;
+	int a;
+	int b;
+	a = 0;
+	b=1;/* comment */
+	var1 = 3;
+	if(a < var1 - 1){
+		a = a*3 + 1;
+		output(assign1(a));
+	}else
+		while ( a == 3 ){
+			int c;
+			c=45;
+			output(c-b);
+		}
+}EOF 
